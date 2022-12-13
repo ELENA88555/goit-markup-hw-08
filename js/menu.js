01d720dda107f7cd894b2f66f6138d0da4fa97b2
Elena Us <Elena88555@gmail.com>
2022-12-13 21:52:49 +0200

    mobile-first

--- js/menu.js
+++ js/menu.js
@@ -2,13 +2,15 @@
   const refs = {
     openMenuBtn: document.querySelector("[data-menu-open]"),
     closMenuBtn: document.querySelector("[data-menu-close]"),
-    modal: document.querySelector("[data-menu]"),
+    menu: document.querySelector("[data-menu]"),
+    body: document.querySelector("body"),
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
   refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
   function toggleMenu() {
-    refs.modal.classList.toggle("is-hidden");
+    refs.menu.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
   }
 })();
